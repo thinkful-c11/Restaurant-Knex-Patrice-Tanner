584563ab5adf5b22aab6394fdf70ede173eb81ee
patricewhite <patricewhite2012@gmail.com>
2017-05-31 12:52:55 -0400

    1-7

--- drills.js
+++ drills.js
@@ -5,7 +5,7 @@ const knex = require('knex')(DATABASE);
 // clear the console before each run
 process.stdout.write('\033c');
 
-// Sample select 
+// Sample select
 // knex.select().from('restaurants')
 //   .debug(true)
 //   .then(results => console.log(results));
@@ -29,6 +29,37 @@ process.stdout.write('\033c');
   //   .debug(true)
   //   .limit(10)
   //   .then(results => console.log(results));
+
+//#4
+// knex.count('id')
+//     .from('restaurants')
+//     .where('cuisine', 'Thai')
+//     .debug(true)
+//     .then(results => console.log(results));
+
+//#5
+// knex.count('id')
+//   .from('restaurants')
+//   .debug(true)
+//   .then(results => console.log(results));
+
+//#6
+// knex.count('id')
+//     .from('restaurants')
+//     .where({cuisine: 'Thai', address_zipcode: 11372})
+//     .debug(true)
+//     .then(results => console.log(results));
+
+//#7
+// knex.select('id', 'name')
+//   .from('restaurants')
+//   .where({cuisine:'Italian', address_zipcode: 10012})
+//   .orWhere({cuisine:'Italian',address_zipcode: 10013})
+//   .orWhere({cuisine:'Italian',address_zipcode: 10014})
+//   .orderBy('name','asc')
+//   .limit(5)
+//   .debug(true)
+//   .then(results => console.log(results));
 
 // Destroy the connection pool
 knex.destroy().then(() => { console.log('closed') });
