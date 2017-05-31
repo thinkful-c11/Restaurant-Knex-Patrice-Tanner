b85c5c3ab779caf13c9608fc2373382f35c6970c
tannaurus <contacttannergill@gmail.com>
2017-05-31 09:15:49 -0700

    first three done, switching over

--- drills.js
+++ drills.js
@@ -6,11 +6,29 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\033c');
 
 // Sample select 
-knex.select().from('restaurants')
-  .debug(true)
-  .then(results => console.log(results));
+// knex.select().from('restaurants')
+//   .debug(true)
+//   .then(results => console.log(results));
 
+// #1
+// knex.select().from('restaurants')
+//   .debug(true)
+//   .then(results => console.log(results));
 
+//#2
+// knex.select()
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .debug(true)
+//   .then(results => console.log(results));
+
+//#3
+  // knex.select('id', 'name')
+  //   .from('restaurants')
+  //   .where('cuisine', 'Italian')
+  //   .debug(true)
+  //   .limit(10)
+  //   .then(results => console.log(results));
 
 // Destroy the connection pool
 knex.destroy().then(() => { console.log('closed') });
